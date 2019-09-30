77515e962805300e25bc207ee02bd833d8f8abb8
Carlos Mico <carlosmico15@gmail.com>
2019-09-30 14:01:36 +0200

    Updates

---
+++ precache-manifest.f9a42d80a8137ee1a1d439a1fec22cb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c12eb9a632c97bd7e2d676fafe3093d",
+    "url": "/WPW/index.html"
+  },
+  {
+    "revision": "e3a62ac2bc044b6c968a",
+    "url": "/WPW/static/css/main.eeb11e67.chunk.css"
+  },
+  {
+    "revision": "d237f1a56dee00335898",
+    "url": "/WPW/static/js/2.b29e561e.chunk.js"
+  },
+  {
+    "revision": "e3a62ac2bc044b6c968a",
+    "url": "/WPW/static/js/main.3a050c55.chunk.js"
+  },
+  {
+    "revision": "5704c474ca64cffc2bf6",
+    "url": "/WPW/static/js/runtime~main.931801d3.js"
+  }
+]);
